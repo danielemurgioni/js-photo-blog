a0f3fbd02add1ecf850ebc2e44b9dce8350bebec
danielemurgioni <dashomew@gmail.com>
2025-04-07 11:24:49 +0200

    [UPD] cicle objects of the API

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 axios.get("https://lanciweb.github.io/demo/api/pictures/").then((resp) => {
-    console.log(resp.data);
+
+    // prendo i dati dell'API
+    const photoInfo = resp.data
+    console.log(photoInfo)
+
+    // ciclo gli oggetti
+    for(let i=0; i<photoInfo.length; i++){
+        console.log(photoInfo[i])
+    }
 });
