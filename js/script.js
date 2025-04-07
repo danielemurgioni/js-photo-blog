3127637dd00c5c1cbf72cd781e8007c471b12878
danielemurgioni <dashomew@gmail.com>
2025-04-07 11:57:21 +0200

    [UPD] created a function to make an html card

--- js/script.js
+++ js/script.js
@@ -6,6 +6,25 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/").then((resp) => {
 
     // ciclo gli oggetti
     for(let i=0; i<photoInfo.length; i++){
-        console.log(photoInfo[i])
+        const photoCard = createPhotoCard(photoInfo[i])
+        console.log(photoCard)
     }
 });
+
+//funzione per creare la struttura html delle photo-card
+const createPhotoCard = (photo) => {
+    
+    //destrutturazione degli oggetti
+    const {id, title, date, url} = photo;
+    
+    //scrittura in html e uso i dati dall'API
+    const photoCard = `<div class="col-12 mt-5">
+                    <div id="photo-card" class="container position-relative">
+                        <img id="pin" class="position-absolute" src="./img/pin.svg">
+                        <img id="photo" class="img-fluid" src="${url}">
+                        <h2 class="mt-4 text-end">${date}</h2>
+                        <h2 class="mt-4 text-end">${title}</h2>
+                    </div>
+                </div>`;
+    return photoCard;
+};
